b5ea35c032beb28fbeacae5d8fb792541696fd5e
drinkmonster <berenyibence98@gmail.com>
2023-07-06 23:16:15 +0200

    main

--- components/Screens/HomeScreen.jsx
+++ components/Screens/HomeScreen.jsx
@@ -1,10 +1,17 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Touchable } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
     <View>
       <Text style={styles.title}>DÖK Helper</Text>
+      <View style={styles.seperator} />
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Program vezető</Text>
+      </View>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Helper</Text>
+      </View>
     </View>
   )
 }
@@ -27,6 +34,21 @@ const styles = StyleSheet.create({
         marginVertical: 8,
         borderBottomColor: '#F2E8CF',
         borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    button: {
+      backgroundColor: '#F2E8CF',
+      width: 300,
+      height: 150,
+      padding: 20,
+      margin: 20,
+      borderRadius: 20,
+      elevation: 3,
+    },
+    buttonText: {
+      fontSize: 40,
+      fontWeight: 'bold',
+      color: '#386641',
+      textAlign: 'center',
     },
 })
 
